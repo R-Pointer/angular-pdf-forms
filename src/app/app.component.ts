354d7d7e4c9442e4a56561d8af5abaccd999a8cd
Pointer <ryan.pointer@sharperlending.com>
2019-12-06 13:14:23 -0800

    Simple Functionality
    The app can load in filled data from the pdf and push it to the json on document load. It can also set updated data to the form. But not save to document. Also started testing pdf-lib.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { PDFDocumentProxy } from 'ng2-pdf-viewer';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { Input } from './input';
+import {PDFDocument} from 'pdf-lib';
 
 @Component({
     selector: 'app-root',
@@ -16,6 +17,11 @@ export class AppComponent {
     public pdfSrc = './pdf-test.pdf';
     public pdfEx = './OoPdfFormExample.pdf';
 
+    // private pdfDoc;
+
+    public jsonURL = './sampleInput.json';
+    public jsonData;
+
     public myForm: FormGroup;
 
     public inputList: Input[] = [];
@@ -24,17 +30,26 @@ export class AppComponent {
 
     constructor(private _fb: FormBuilder) {
         this.myForm = this._fb.group({});
-     }
+        // this.setupPDF();
+     }// end constructor
+
+    /*private async setupPDF() {
+      const pdfMeta = await fetch(this.pdfSrc).then(res => res.arrayBuffer());
+      let pages;
+      this.pdfDoc = await PDFDocument.load(pdfMeta).then(res => pages = res.getPages());
+      console.log(this.pdfDoc);
+      console.log(pages[0]);
+    }// end setupPDF*/
 
     private createInput(annotation: PDFAnnotationData, rect: number[] = null) {
         let formControl = new FormControl(annotation.buttonValue || '');
-
         const input = new Input();
         input.name = annotation.fieldName;
 
         if (annotation.fieldType === 'Tx') {
             input.type = 'text';
-            input.value = annotation.buttonValue || '';
+            input.value = annotation.fieldValue || '';
+            formControl = new FormControl(input.value);
         }
 
         if (annotation.fieldType === 'Btn' && !annotation.checkBox) {
@@ -44,8 +59,13 @@ export class AppComponent {
 
         if (annotation.checkBox) {
             input.type = 'checkbox';
-            input.value = true;
-            formControl = new FormControl(annotation.buttonValue || false);
+            input.value = annotation.fieldValue === 'Yes' ? true : false;
+            formControl = new FormControl(input.value || false);
+        }
+
+        if (annotation.fieldType === 'Ch') {
+          input.type = 'text';
+          input.value = annotation.fieldValue || '';
         }
 
         // Calculate all the positions and sizes
@@ -55,7 +75,6 @@ export class AppComponent {
             input.height = (rect[1] - rect[3]) * .9;
             input.width = (rect[2] - rect[0]);
         }
-
         this.inputList.push(input);
         return formControl;
     }
@@ -98,12 +117,11 @@ export class AppComponent {
 
     public loadComplete(pdf: PDFDocumentProxy): void {
         for (let i = 1; i <= pdf.numPages; i++) {
-
             // track the current page
             let currentPage = null;
             pdf.getPage(i).then(p => {
                 currentPage = p;
-
+                console.log(p);
                 // get the annotations of the current page
                 return p.getAnnotations();
             }).then(ann => {
@@ -111,7 +129,7 @@ export class AppComponent {
                 // ugly cast due to missing typescript definitions
                 // please contribute to complete @types/pdfjs-dist
                 const annotations = (<any>ann) as PDFAnnotationData[];
-
+                console.log(annotations);
                 annotations
                     .filter(a => a.subtype === 'Widget') // get the form field annotation only
                     .forEach(a => {
@@ -126,5 +144,22 @@ export class AppComponent {
                     });
             });
         }
+        console.log(this.myForm);
+        console.log(this.inputList);
+    }// end loadComplete
+
+    public logJSON() {
+        console.log(this.myForm.value);
+        // console.log(this.pdfDoc);
     }
-}
+
+    public saveToPDF() {
+      console.log('saving to pdf');
+      this.myForm.setValue({name: this.inputList[0].value,
+                                  surname: this.inputList[1].value,
+                                  email: this.inputList[2].value,
+                                  gender: '',
+                                  angular: this.inputList[5].value, react: this.inputList[6].value, vue: this.inputList[7].value});
+    }// end saveToPDF
+
+}// end AppComponent
